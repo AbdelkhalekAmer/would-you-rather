0d528d41857ee60287d99f7052c9bb47ce21cd71
Abdelkhalek <abdelkhalekamer@outlook.com>
2021-10-17 23:53:20 +0200

    fix bug

--- src/Store/question-actions.js
+++ src/Store/question-actions.js
@@ -1,3 +1,4 @@
+import { userActions } from './user-slice';
 import { questionActions } from "./question-slice";
 import * as questionService from '../Services/QuestionService';
 
@@ -32,10 +33,15 @@ export const createQuestion = (question) => {
 export const saveAnswer = (authenticatedUserId, questionId, answer) => {
     return async (dispatch) => {
         try {
-            const rawQuestions = await questionService.saveAnswer(authenticatedUserId, questionId, answer);
+            const { questions: rawQuestions, users: rawUsers } = await questionService.saveAnswer(authenticatedUserId, questionId, answer);
+
             const questions = [];
             for (const key in rawQuestions) questions.push(rawQuestions[key]);
             dispatch(questionActions.updateQuestionsWithAnswers({ questions }));
+
+            const users = [];
+            for (const key in rawUsers) users.push(rawUsers[key]);
+            dispatch(userActions.setUsers({ users }));
         } catch (error) {
             //TODO: to be replaced by a notification service or notification reducer.
             alert(error);
