4d3c046d43651ec26fc372b5dcedc7f6bd674bcd
Abdelkhalek <abdelkhalekamer@outlook.com>
2021-10-26 01:58:42 +0200

    revamp UI

--- src/Components/Question/QuestionToAnswer.js
+++ src/Components/Question/QuestionToAnswer.js
@@ -1,16 +1,18 @@
 import './QuestionToAnswer.css';
+import StaticCard from '../UI/StaticCard/StaticCard';
 import User from '../User/User';
 import { Link, useHistory } from 'react-router-dom';
 
 const QuestionToAnswer = ({ author, question }) => {
     const history = useHistory();
 
-    return (<div className="question">
-        <Link to={{ pathname: `/questions/${question.id}`, state: { from: history.location } }}>
-            <User {...author} />
+    return (<StaticCard>
+        <User {...author} />
+        <div className='question'>
             <p>{question.text}</p>
-        </Link >
-    </div>);
+            <Link to={{ pathname: `/questions/${question.id}`, state: { from: history.location } }}>View</Link>
+        </div>
+    </StaticCard>);
 };
 
 export default QuestionToAnswer;
